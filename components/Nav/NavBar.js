627e0390f9e4f58d875b9f68ddcf4bd1f0b3739c
PaulHaze <dev@artemist.com.au>
2021-09-16 06:56:32 +1000

    add icons and fix icon placement

--- components/Nav/NavBar.js
+++ components/Nav/NavBar.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 
-import {
-  ChevronDoubleLeftIcon,
-  ChevronDoubleRightIcon,
-} from '@heroicons/react/solid';
+import { BiChevronsLeft, BiChevronsRight } from 'react-icons/bi';
 
 import { NavLink } from './NavLink';
 
@@ -21,14 +18,14 @@ export function NavBar() {
     <nav className="h-[100vh] p-5 px-2 py-4 bg-gradient-to-b from-indigo-300/90 to-sky-800/90">
       <div className="flex">
         {showHideNav ? (
-          <ChevronDoubleLeftIcon
+          <BiChevronsLeft
             onClick={handleNavClick}
             className="w-6 h-6 text-blue-500 bg-[rgba(255,255,255,0.9)] border-2 border-blue-500 rounded"
           />
         ) : (
-          <ChevronDoubleRightIcon
+          <BiChevronsRight
             onClick={handleNavClick}
-            className="w-6 h-6 text-blue-500 border-2 bg-[rgba(255,255,255,0.9)] border-blue-500 rounded"
+            className="w-6 h-6 pl-[1px] text-blue-500 border-2 bg-[rgba(255,255,255,0.9)] border-blue-500 rounded"
           />
         )}
       </div>
