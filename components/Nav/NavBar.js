34c9850cfbd9dc0ee9dcddfd4ee7a0efc585a7a4
PaulHaze <dev@artemist.com.au>
2022-01-24 09:43:18 +1100

    add animation to side bar component

--- components/Nav/NavBar.js
+++ components/Nav/NavBar.js
@@ -7,17 +7,22 @@ import { NavLink } from './NavLink';
 import { routes } from '../../routes';
 
 export function NavBar() {
-  const [showHideNav, setShowHideNav] = useState(true);
+  const [showNav, setShowNav] = useState(true);
 
-  const handleNavClick = () => setShowHideNav(prev => !prev);
+  const handleNavClick = () => setShowNav(prev => !prev);
 
   const renderRoutes = routes.map(r => (
     <NavLink key={r.key} linkRef={r.path} linkText={r.label} />
   ));
   return (
-    <nav className="min-h-screen sticky top-0 p-5 px-2 py-4 bg-gradient-to-b from-indigo-300/90 to-sky-800/90">
-      <div className="flex w-full">
-        {showHideNav ? (
+    <nav
+      className={`min-h-screen relative top-0 ${
+        showNav ? 'left-0' : '-left-[75px]'
+      } ease-in-out duration-300 p-5 px-2 py-4 bg-gradient-to-b from-indigo-300/90 to-sky-800/90 `}
+    >
+      {/* SHOW HIDE BUTTON */}
+      <div className="flex justify-end">
+        {showNav ? (
           <BiChevronsLeft
             onClick={handleNavClick}
             className="w-6 h-6 text-blue-500 bg-[rgba(255,255,255,0.9)] border-2 border-blue-500 rounded"
@@ -29,7 +34,14 @@ export function NavBar() {
           />
         )}
       </div>
-      <ul className={`w-full ${!showHideNav && 'hidden'}`}>{renderRoutes}</ul>
+      {/* ROUTES */}
+      <ul
+        className={`opacity-1 transition-all duration-500 ${
+          !showNav && 'opacity-0'
+        }`}
+      >
+        {renderRoutes}
+      </ul>
     </nav>
   );
 }
